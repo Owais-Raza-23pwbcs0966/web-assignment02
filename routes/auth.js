4bdf5aa7030f7c77d4776c7c1c6be6d500c96fe7
Owais-Raza-23pwbcs0966 <23pwbcs0966@uetpeshawar.edu.pk>
2025-01-09 15:45:44 +0500

    updated a route

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,10 @@ const auth = require('../middleware/auth');
 
 const router = express.Router();
 
+router.post('/', async (req, res) => {
+    res.status(201).json("the server is running");
+})
+
 // POST /api/signup
 router.post('/signup', async (req, res) => {
     try {
